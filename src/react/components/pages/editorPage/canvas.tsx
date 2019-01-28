25d1356a43b8b1c031b8e6ce5d668635dd21866c
Tom Miller <tmiller@microsoft.com>
2019-01-28 00:20:48 -0800

    Initial checkin

--- src/react/components/pages/editorPage/canvas.tsx
+++ src/react/components/pages/editorPage/canvas.tsx
@@ -276,8 +276,9 @@ export default class Canvas extends React.Component<ICanvasProps, ICanvasState> 
                 <div id="selection-zone" className={`asset-${this.getAssetType()}`}>
                     <div id="editor-zone" className="full-size" />
                 </div>
-            </div>
-        );
+            }
+            </React.Fragment>
+            );
     }
 
     /**
